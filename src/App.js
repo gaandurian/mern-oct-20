4d81e86d6ec15b29c3fa3def5d15aa1e0abef74c
Mahmoud Ghandour <ghan.mahmoud@gmail.com>
2020-10-19 21:49:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [batata, setBatata] = useState(0);
+  const [views, setViews] = useState(0);
+
+  // setTimeout(() => {
+  //   setViews(Math.floor(Math.random() * 1000))
+  // }, 2000)
+
+  useEffect(() => {
+    // this will fire every one second until random_number is greater than 600
+    const myInterval = setInterval(() => {
+      const random_number = Math.floor(Math.random() * 1000);
+      setBatata(random_number);
+
+      if (random_number > 600) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+
+    // this will fire once after two seconds
+    setTimeout(() => {
+      setViews(Math.floor(Math.random() * 300));
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ paddingLeft: 300, paddingTop: 100 }}>
+      <h3> Quantity of batata: {batata}</h3>
+      <h3> page views: {views} </h3>
     </div>
   );
 }
-
-export default App;
